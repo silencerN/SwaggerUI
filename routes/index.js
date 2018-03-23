f6a25ee2f3fb9ea8ddd452b6d3bdfe5d5f7bd471
Liam <411795008@qq.com>
2018-03-23 13:43:56 +0800

    add navigation

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.redirect('/Swagger/index.html')
+  res.render('index');
 });
 module.exports = router;
